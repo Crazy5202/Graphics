3d0af52b50f34c203b1ea5068f61d93371ef3b64
maxim <mdkutsenko@mai.education>
2024-10-07 22:50:00 +0300

    Fixed random movement

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -3,10 +3,10 @@
 #include <random>
 
 int main() {
+    sf::Clock clock;
     std::random_device rd; // Разные штуки для обеспечения случайного движения ломаной
     std::mt19937_64 gen(rd());
     std::uniform_int_distribution<int> dis_elem(-30, 30);
-    sf::Clock clock;
 
     sf::VideoMode desktop = sf::VideoMode::getDesktopMode(); // создание окна
     sf::RenderWindow window(sf::VideoMode(desktop.width, desktop.height), "Program window");
@@ -16,12 +16,15 @@ int main() {
     std::vector<int> edges; // вектор отрезков ломаной
     bool random_movement = false; // флаг случайного движения
     int mov_ind = -1; // индекс вершины в векторе, которую мы двигаем
+
     while (window.isOpen()) { // игровой цикл
         sf::Event event;
+        
         while (window.pollEvent(event)) { // цикл событий
             if (event.type == sf::Event::Closed) { // закрытие окна
                 window.close();
             }
+
             if (event.type == sf::Event::MouseButtonPressed) { // нажали клавишу мыши
                 if (event.mouseButton.button == sf::Mouse::Button::Right) { // правая клавиша мыши - создаём вершину (и отрезок)
                     sf::CircleShape point(20.f);
@@ -31,6 +34,7 @@ int main() {
                     point.setPosition(event.mouseButton.x, event.mouseButton.y);
                     points.push_back(point);
                 }
+
                 if (event.mouseButton.button == sf::Mouse::Button::Left) { // левая клавиша мыши - "захватываем" вершину для движения
                     for (int i=0; i<points.size(); ++i) {
                         if (points[i].getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
@@ -39,6 +43,7 @@ int main() {
                         } 
                     }
                 }
+
                 if (event.mouseButton.button == sf::Mouse::Button::Middle) { // средняя клавиша мыши - удаляем вершину
                     for (auto it=points.begin(); it!=points.end(); ++it) {
                         if (it->getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
@@ -48,31 +53,40 @@ int main() {
                     }
                 }
             }
+
             if (event.type == sf::Event::MouseMoved and mov_ind != -1) { // двигается мышь -> если "захвачена" вершина, двигаем и её
                 points[mov_ind].setPosition(event.mouseMove.x, event.mouseMove.y);
             }
+
             if (event.type == sf::Event::MouseButtonReleased and event.mouseButton.button == sf::Mouse::Button::Left) { // отпускаем левую клавишу мыши - а с ней и вершину
                 mov_ind = -1;
             } 
+
             if (event.type == sf::Event::KeyPressed and event.key.code == sf::Keyboard::Space) { // при нажатии пробела включаем случайное движение ломаной
                 random_movement = true;
             }
+
             if (event.type == sf::Event::KeyReleased and event.key.code == sf::Keyboard::Space) { // при отпускании пробела отключаем случайное движение ломаной
                 random_movement = false;
             }
         }
+
         window.clear(); // очищаем экран
-        if (random_movement and (clock.getElapsedTime().asMilliseconds() >= 200)) { // цикл случайного движения, в нём делаем чтобы вершину не выходили за пределы экрана
+
+        if (random_movement and (clock.getElapsedTime().asMilliseconds() >= 200)) { // цикл случайного движения, в нём делаем чтобы вершины не выходили за пределы экрана
             for (int i=0; i<points.size(); ++i) {
                 int new_x = (static_cast<int>(points[i].getPosition().x) + dis_elem(gen)) % desktop.width;
-                int new_y = (static_cast<int>(points[i].getPosition().x) + dis_elem(gen)) % desktop.height;
+                int new_y = (static_cast<int>(points[i].getPosition().y) + dis_elem(gen)) % desktop.height;
                 points[i].setPosition(new_x, new_y);
             }
+
             clock.restart();
         }
+
         for (int i=0; i<points.size(); ++i) { // отрисовываем всё на экране
             window.draw(points[i]);
         }
+
         window.display(); // выводим изображение
     }
     return 0;
